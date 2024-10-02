7bc230292ad8fb14338b1ddceed2ddfde38ba9c2
BatuhanCaglar <fbmanbc@gmail.com>
2024-10-02 12:53:45 -0400

    ad

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const DOMSelectors = {
   header: document.querySelector("h1"),
   subheader: document.querySelector("H2"),
+  button: document.querySelector(".btn"),
+  form: document.querySelector(".form"),
+  inputcard: document.querySelector(".input-card"),
 };
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(document.querySelector("input").value);
+});
